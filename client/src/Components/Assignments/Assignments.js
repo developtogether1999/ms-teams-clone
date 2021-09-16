27339c2e7518660804bc0140f767341a608fe92d
thespartonguru <gurumanglam035@gmail.com>
2021-09-16 16:25:53 +0530

    Revert "assignments refactor"
    This reverts commit d53caec2b9e9753cb1aa8b480b61c97a92503520.

--- client/src/Components/Assignments/Assignments.js
+++ client/src/Components/Assignments/Assignments.js
@@ -1,65 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Button from 'react-bootstrap/Button'
-import PendingBlogList from "./PendingBlogList";
-import './Assignments.css';
-import data from './data';
-import CompletedBlogList from "./CompletedBloglist";
-  
+import React from "react";
 
+const Assignments = () => {
 
-
-const Assignments = (props) => {
-
-
-const [blogs1, setBlogs1] = useState(null);
-const [blogs2, setBlogs2] = useState(null);
-const [flag1, setFlag1] = useState(true);
-const [flag2, setFlag2] = useState(true);
-
-    return ( 
+    return (
         <>
-         
-                <Button id="b1" onClick={()=>{
-                    
-                    if(flag1===true){
-                       setBlogs1(data);
-                       setFlag1(false);
-                       setBlogs2(null);
-                       setFlag2(true);
-
-                    }
-                    else{
-                     setBlogs1(null);
-                     setFlag1(true);
-                    }
-                
-                
-                }}>
-                    Pending Assignments
-                     {blogs1&&<PendingBlogList blogs={blogs1}  />}
-                    
-                </Button>
-                <Button id="b2" onClick={()=>{
-                    
-                      if(flag2===true){
-                           
-                        setBlogs2(data);
-                        setFlag2(false);
-                        setBlogs1(null);
-                        setFlag1(true);
-                      
-                    }
-                    else{
-                      setBlogs2(null);
-                     setFlag2(true);
-                    }
-                }}>
-                    Completed Assignments
-                     {blogs2&&<CompletedBlogList blogs={blogs2} />}
-                </Button>
-
-      </>  
-        
+            Assignments
+        </>
      );
 }
 
